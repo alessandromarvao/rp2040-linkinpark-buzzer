b7a2d501723c33766b739ec3e59591793f077d75
Alessandro Marvão <alessandromarvao@Aspire-A315-53-dc08b725.localdomain>
2025-02-17 22:58:09 -0300

    Fazendo o teste tocando Linkin Park

--- buzzer_test.c
+++ buzzer_test.c
@@ -4,16 +4,21 @@
 #include "hardware/clocks.h"         // Biblioteca para controle de clocks
 
 // Configuração do pino do buzzer
-#define BUZZER_PIN 21                // Define o pino 21 como o pino de controle do buzzer
+#define BUZZER_PIN_1 21                // Define o pino 21 como o pino de controle do buzzer 1
+#define BUZZER_PIN_2 10                // Define o pino 10 como o pino de controle do buzzer 2
 
 // Notas musicais para a música tema de Star Wars (Frequências das notas musicais em Hz)
-const uint star_wars_notes[] = {
-    330, 262, 262, 415, 392, 392, 392, 392, 415
+const uint notes_1[] = {
+    767, 767, 767, 767, 750, 750, 750, 750,750, 730, 730, 730, 730, 750, 750, 750, 750, 750
 };
 
 // Duração das notas em milissegundos
 const uint note_duration[] = {
-    536, 536, 536, 536, 536, 536, 536, 268, 268
+    536, 536, 536, 536, 536, 536, 536, 268, 268, 536, 536, 536, 536, 536, 536, 536, 268, 268,
+};
+// Notas musicais para a música tema de Star Wars (Frequências das notas musicais em Hz)
+const uint notes_2[] = {
+    383, 410, 410, 396, 392, 392, 392, 392, 396, 383, 410, 410, 396, 392, 392, 392, 392, 396
 };
 
 // Inicializa o PWM no pino do buzzer
@@ -33,7 +38,7 @@ void pwm_init_buzzer(uint pin) {
 }
 
 // Toca uma nota com a frequência e duração especificadas
-void play_tone(uint pin, uint frequency, uint duration_ms) {
+void play_tone(uint pin, float frequency, uint duration_ms) {
     // Obtém o número do slice do pino
     uint slice_num = pwm_gpio_to_slice_num(pin);
     // Obtém a frequência do clock do sistema
@@ -56,10 +61,11 @@ void play_tone(uint pin, uint frequency, uint duration_ms) {
 }
 
 // Função principal para tocar a música
-void play_song(uint pin) {
+void play_song() {
     // Loop através das notas e durações da música
-    for (int i = 0; i < sizeof(star_wars_notes) / sizeof(star_wars_notes[0]); i++) {
-        play_tone(pin, star_wars_notes[i], note_duration[i]);
+    for (int i = 0; i < sizeof(notes_1) / sizeof(notes_1[0]); i++) {
+        // play_tone(BUZZER_PIN_1, notes_1[i], note_duration_1[i]);
+        play_tone(BUZZER_PIN_2, notes_2[i], note_duration[i]);
     }
 }
 
@@ -67,11 +73,12 @@ int main() {
     // Inicializa a comunicação serial
     stdio_init_all();
     // Inicializa o buzzer no pino definido
-    pwm_init_buzzer(BUZZER_PIN);
+    pwm_init_buzzer(BUZZER_PIN_1);
+    pwm_init_buzzer(BUZZER_PIN_2);
 
     while (1) {
         // Toca a música do Star Wars repetidamente
-        play_song(BUZZER_PIN);
+        play_song();
     }
 
     return 0;
